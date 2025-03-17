e326f68a9bc975a05ecf8df1f6cc08b00423a052
Oslynx <malakayalvg@gmail.com>
2025-03-17 15:22:51 +0100

    page routing + css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import './css/App.css'
 import Home from "./pages/Home.jsx";
+import {Route, Routes} from "react-router-dom";
+import Favorites from "./pages/Favorites.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
   return (
     <>
-        <Home/>
+        <div>
+            <Navbar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     </>
   )
 }
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,13 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+
+    return(
+        <div className="favorite-empty">
+            <h2>No favorite Movies yet</h2>
+            <p>Start adding your favorite movies now !</p>
+        </div>
+    )
+}
+
+export default Favorites;
